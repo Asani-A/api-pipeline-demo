6fa0acb0a3c0947eeabfa6647e8ffa79434347aa
Abdul-Jaleel Asani <abduljaleelasani@gmail.com>
2026-01-20 15:21:01 +0200

    feat: add swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';                  
+import path from 'path';
 import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 
 const app = express();
 
+// We use path.join to ensure it works in both 'src' (dev) and 'dist' (prod)
+const swaggerDocument = YAML.load(path.join(__dirname, './docs/swagger.yaml'));
+
 // Middleware
 app.use(express.json());
+
+// Serve Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Mount the User Routes
 app.use('/users', userRoutes);
